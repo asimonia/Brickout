87973b0629ee3b05c2dff6c0172a14cc41223e3c
asimonia <alex.simonian@gmail.com>
2016-08-03 20:32:38 -0500

    Add Mixin clas

--- breakout.py
+++ breakout.py
@@ -15,6 +15,34 @@ class Game(tk.Frame):
 		self.pack()
 
 
+class GameObjectMixin:
+	"""Ball, Paddle and Brick inherit from this Mixin"""
+	def __iniit__(self, canvas, item):
+		self.canvas = canvas
+		self.item = item
+
+	def get_position(self):
+		return self.canvas.coords(self.item)
+
+	def move(self, x, y):
+		self.canvas.move(self.item, x, y)
+
+	def delete(self):
+		self.canvas.delete(self.item)
+
+
+class Ball(GameObjectMixin):
+	pass
+
+
+class Paddle(GameObjectMixin):
+	pass
+
+
+class Brick(GameObjectMixin):
+	pass
+
+
 if __name__ == '__main__':
 	root = tk.Tk()
 	root.title("Breakout")
