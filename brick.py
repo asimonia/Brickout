9a2199a8dc147c3b0b8896181fc99a122d207c5d
asimonia <alex.simonian@gmail.com>
2017-03-15 15:13:49 -0500

    Add initializer and hit method

--- brick.py
+++ brick.py
@@ -1,1 +1,28 @@
 from gameobject import GameObject
+
+class Brick(GameObject):
+
+	COLORS = {1: '#999999', 2: '#555555', 3: '#222222'}
+
+	def __init__(self, canvas, x, y, hits):
+		self.width = 75
+		self.height = 20
+		self.hits = hits
+		color = Brick.COLORS[hits]
+		item = canvas.create_rectange(x - self.width / 2,
+									  y - self.height / 2,
+									  x + self.width / 2,
+									  y + self.height / 2,
+									  fill=color,
+									  tags='brick')
+		super().__init__(canvas, item)
+
+	def hit(self):
+		"""Decrease paddle hits when hit, until it disappears.
+		   Change the color of the brick when hit.
+		"""
+		self.hits -= 1
+		if self.hits == 0:
+			self.delete()
+		else:
+			self.canvas.itemconfig(self.item, fill=Brick.COLORS[self.hits])
